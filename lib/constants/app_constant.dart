a52172b3e54a924ce0ce5f94a7dd4e4c1a1865a5
danakoshen <114384592+danakoshen@users.noreply.github.com>
2024-07-05 16:56:41 +0900

    upd

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,13 +1,13 @@
 import 'package:mindsight_admin_page/routes/app_routes.dart';
 
 class AppConstant {
-  static bool isProdServer = false;
+  static bool isProdServer = true;
   static String version = "1.0.6";
   // static const String initialRoute = AppRoutes.bottomNavi;
   static const String initialRoute = AppRoutes.auth;
 
   // 테스트 용
-  static bool chleesTest = false;
+  static bool chleesTest = true;
   // static String chleesTestEmail = "test@gmail.com";
   // static String chleesTestPassword = "123";
   // static String chleesTestEmail = "chlee@nodamen.com";
