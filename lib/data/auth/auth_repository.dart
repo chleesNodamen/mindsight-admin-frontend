fa1ffe823b2d2583f74f8d135ba751b807d498ff
danakoshen <114384592+danakoshen@users.noreply.github.com>
2024-07-05 13:05:46 +0900

    upd

--- lib/data/auth/auth_repository.dart
+++ lib/data/auth/auth_repository.dart
@@ -8,10 +8,6 @@ class AuthRepository extends BaseRepository {
     // req
     String endpoint = "admin/signin";
     Response response = await httpClient.post(endpoint, dto.toJson());
-    print(dto.email);
-    print(dto.password);
-    print(response.body);
-    print(response.statusCode);
 
     // result
     AuthModel model = fetchJsonData<AuthModel>(response, AuthModel.fromJson);
