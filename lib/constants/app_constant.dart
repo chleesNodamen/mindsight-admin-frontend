cd8d049b919d7605c375cb75488f9dfbbfcfb3a8
chlees <162085989+chleesNodamen@users.noreply.github.com>
2025-03-10 17:09:21 +0900

    1111

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,5 +1,5 @@
 class AppConstant {
-  static int version = 24;
+  static int version = 25;
   static bool testClearPref = false;
   static String testEmail = "hundrill@naver.com";
   static String testPassword = "1111";
